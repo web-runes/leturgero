5a96c7f2661170aa3d781f295685d9a572f011e3
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-05-29 10:51:22 +0200

    feat: add agent instructions

---
+++ packages/cli/src/commands/main.ts
@@ -0,0 +1,166 @@
+import { createReadStream } from "node:fs";
+import { writeFile } from "node:fs/promises";
+import { styleText } from "node:util";
+import { confirm, intro, note, outro, stream } from "@clack/prompts";
+import pkg from "../../package.json" with { type: "json" };
+import { generateCss } from "../core/generate-css.js";
+import { proxySources } from "../core/proxy-sources.js";
+import { saveCssToDisk } from "../core/save-css-to-disk.js";
+import { saveFontsToDisk } from "../core/save-fonts-to-disk.js";
+import { selectCssVariable } from "../core/select-css-variable.js";
+import { selectFamily } from "../core/select-family.js";
+import { selectPaths } from "../core/select-paths.js";
+import { selectProperties } from "../core/select-properties.js";
+import { ClackAutocomplete } from "../infra/clack-autocomplete.js";
+import { ClackDirectoryPicker } from "../infra/clack-directory-picker.js";
+import {
+	ClackCancelError,
+	ClackErrorHandler,
+} from "../infra/clack-error-handler.js";
+import { ClackLogger } from "../infra/clack-logger.js";
+import { ClackMultiselect } from "../infra/clack-multiselect.js";
+import { ClackProgress } from "../infra/clack-progress.js";
+import { ClackSpinner } from "../infra/clack-spinner.js";
+import { ClackText } from "../infra/clack-text.js";
+import { CryptoHasher } from "../infra/crypto-hasher.js";
+import { FuseSearch } from "../infra/fuse-search.js";
+import { UnifontFontsManager } from "../infra/unifont-fonts-manager.js";
+
+export async function mainImpl(): Promise<void> {
+	const outroMessage = `Thanks for using our tool! We'd love your feedback: ${styleText("blue", "https://github.com/web-runes/leturgero/issues")}`;
+	const errorHandler = new ClackErrorHandler({ outroMessage });
+	try {
+		const createSpinner = () => new ClackSpinner();
+		const createAutocomplete = () => new ClackAutocomplete();
+		const createMultiselect = () => new ClackMultiselect();
+		const createDirectoryPicker = () => new ClackDirectoryPicker();
+		const createProgress = (max: number) => new ClackProgress({ max });
+		const createText = () => new ClackText();
+		const logger = new ClackLogger();
+		const hasher = new CryptoHasher();
+
+		intro(
+			`Welcome to ${styleText("bgGreen", ` ${pkg.name} `)} ${styleText("green", `v${pkg.version}`)}!`,
+		);
+
+		await stream.message(
+			createReadStream(new URL("../../logo.txt", import.meta.url), {
+				encoding: "utf-8",
+			}),
+		);
+
+		const initSpinner = createSpinner();
+		initSpinner.start("Initializing...");
+		const fontsManager = await UnifontFontsManager.create();
+		initSpinner.stop("Initialized");
+
+		const root = process.cwd();
+
+		const paths = await selectPaths({
+			directoryPicker: createDirectoryPicker(),
+			root,
+			text: createText(),
+		});
+
+		const family = await selectFamily({
+			fontsManager,
+			autocomplete: createAutocomplete(),
+			createSearch: (items) => new FuseSearch(items, ["name"]),
+		});
+
+		const suggestions = await fontsManager.getSuggestions(family);
+
+		const properties = await selectProperties({
+			suggestions,
+			createMultiselect,
+			logger,
+		});
+
+		const resolveSpinner = createSpinner();
+		resolveSpinner.start("Retrieving font data...");
+		const resolved = await fontsManager.resolve(family, properties);
+		resolveSpinner.stop("Retrieved");
+
+		let fonts = resolved.fonts;
+
+		if (fonts.length === 0) {
+			logger.warn(
+				"No fonts could be found for this combo of weights, styles, subsets and formats. Retry with another combination",
+			);
+			return;
+		}
+
+		const cssVariable = await selectCssVariable({
+			family: family.name,
+			text: createText(),
+		});
+
+		const total = fonts.reduce((acc, font) => {
+			return acc + font.src.filter((src) => !("name" in src)).length;
+		}, 0);
+
+		if (
+			(await confirm({
+				message: `${total} file${total === 1 ? "" : "s"} will be downloaded. Do you want to continue?`,
+			})) !== true
+		) {
+			throw new ClackCancelError();
+		}
+
+		const proxyResult = await proxySources({
+			cssVariable,
+			fonts,
+			publicDir: paths.publicDir,
+			publicFontsDir: paths.publicFontsDir,
+			hasher,
+			createProgress: () => createProgress(total),
+			createCancelError: () => new ClackCancelError(),
+			fetch: (url) =>
+				fetch(url)
+					.then((res) => res.arrayBuffer())
+					.then((e) => Buffer.from(e)),
+		});
+
+		fonts = proxyResult.fonts;
+
+		await saveFontsToDisk({
+			filenameToContents: proxyResult.filenameToContents,
+			publicDir: paths.publicDir,
+			publicFontsDir: paths.publicFontsDir,
+			writeFile,
+			logger,
+		});
+
+		// TODO: ask for fallbacks (use resolved.fallbacks)
+		const fallbacks = resolved.fallbacks ?? [];
+
+		// TODO: optimized fallbacks
+
+		const css = generateCss({
+			cssVariable,
+			fallbacks,
+			family: family.name,
+			fonts,
+		});
+
+		await saveCssToDisk({
+			css,
+			cssVariable,
+			logger,
+			stylesDir: paths.stylesDir,
+			writeFile,
+		});
+
+		note(
+			[
+				"Now that you have font and CSS files, it is time to hook them up in your project.",
+				`Head over to the documentation to learn how: ${styleText("blue", "TODO:")}`,
+			].join("\n"),
+			"Next steps",
+		);
+
+		outro(outroMessage);
+	} catch (error) {
+		errorHandler.onError(error);
+	}
+}
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,31 +1,7 @@
-import { createReadStream } from "node:fs";
-import { writeFile } from "node:fs/promises";
-import { styleText } from "node:util";
-import { confirm, intro, note, outro, stream } from "@clack/prompts";
+import { note } from "@clack/prompts";
+import { isAgent } from "am-i-vibing";
 import { defineCommand, runMain } from "citty";
 import pkg from "../package.json" with { type: "json" };
-import { generateCss } from "./core/generate-css.js";
-import { proxySources } from "./core/proxy-sources.js";
-import { saveCssToDisk } from "./core/save-css-to-disk.js";
-import { saveFontsToDisk } from "./core/save-fonts-to-disk.js";
-import { selectCssVariable } from "./core/select-css-variable.js";
-import { selectFamily } from "./core/select-family.js";
-import { selectPaths } from "./core/select-paths.js";
-import { selectProperties } from "./core/select-properties.js";
-import { ClackAutocomplete } from "./infra/clack-autocomplete.js";
-import { ClackDirectoryPicker } from "./infra/clack-directory-picker.js";
-import {
-	ClackCancelError,
-	ClackErrorHandler,
-} from "./infra/clack-error-handler.js";
-import { ClackLogger } from "./infra/clack-logger.js";
-import { ClackMultiselect } from "./infra/clack-multiselect.js";
-import { ClackProgress } from "./infra/clack-progress.js";
-import { ClackSpinner } from "./infra/clack-spinner.js";
-import { ClackText } from "./infra/clack-text.js";
-import { CryptoHasher } from "./infra/crypto-hasher.js";
-import { FuseSearch } from "./infra/fuse-search.js";
-import { UnifontFontsManager } from "./infra/unifont-fonts-manager.js";
 
 const main = defineCommand({
 	meta: {
@@ -33,147 +9,22 @@ const main = defineCommand({
 		description: pkg.description,
 		version: pkg.version,
 	},
-	// TODO: implement flags
-	args: {},
-	// TODO: detect agent usage and abort with some kind of help
-	// may need splitting some features in several commands
 	async run() {
-		const outroMessage = `Thanks for using our tool! We'd love your feedback: ${styleText("blue", "https://github.com/web-runes/leturgero/issues")}`;
-		const errorHandler = new ClackErrorHandler({ outroMessage });
-		try {
-			const createSpinner = () => new ClackSpinner();
-			const createAutocomplete = () => new ClackAutocomplete();
-			const createMultiselect = () => new ClackMultiselect();
-			const createDirectoryPicker = () => new ClackDirectoryPicker();
-			const createProgress = (max: number) => new ClackProgress({ max });
-			const createText = () => new ClackText();
-			const logger = new ClackLogger();
-			const hasher = new CryptoHasher();
-
-			intro(
-				`Welcome to ${styleText("bgGreen", ` ${pkg.name} `)} ${styleText("green", `v${pkg.version}`)}!`,
-			);
-
-			await stream.message(
-				createReadStream(new URL("../logo.txt", import.meta.url), {
-					encoding: "utf-8",
-				}),
-			);
-
-			const initSpinner = createSpinner();
-			initSpinner.start("Initializing...");
-			const fontsManager = await UnifontFontsManager.create();
-			initSpinner.stop("Initialized");
-
-			const root = process.cwd();
-
-			const paths = await selectPaths({
-				directoryPicker: createDirectoryPicker(),
-				root,
-				text: createText(),
-			});
-
-			const family = await selectFamily({
-				fontsManager,
-				autocomplete: createAutocomplete(),
-				createSearch: (items) => new FuseSearch(items, ["name"]),
-			});
-
-			const suggestions = await fontsManager.getSuggestions(family);
-
-			const properties = await selectProperties({
-				suggestions,
-				createMultiselect,
-				logger,
-			});
-
-			const resolveSpinner = createSpinner();
-			resolveSpinner.start("Retrieving font data...");
-			const resolved = await fontsManager.resolve(family, properties);
-			resolveSpinner.stop("Retrieved");
-
-			let fonts = resolved.fonts;
-
-			if (fonts.length === 0) {
-				logger.warn(
-					"No fonts could be found for this combo of weights, styles, subsets and formats. Retry with another combination",
-				);
-				return;
-			}
-
-			const cssVariable = await selectCssVariable({
-				family: family.name,
-				text: createText(),
-			});
-
-			const total = fonts.reduce((acc, font) => {
-				return acc + font.src.filter((src) => !("name" in src)).length;
-			}, 0);
-
-			if (
-				(await confirm({
-					message: `${total} file${total === 1 ? "" : "s"} will be downloaded. Do you want to continue?`,
-				})) !== true
-			) {
-				throw new ClackCancelError();
-			}
-
-			const proxyResult = await proxySources({
-				cssVariable,
-				fonts,
-				publicDir: paths.publicDir,
-				publicFontsDir: paths.publicFontsDir,
-				hasher,
-				createProgress: () => createProgress(total),
-				createCancelError: () => new ClackCancelError(),
-				fetch: (url) =>
-					fetch(url)
-						.then((res) => res.arrayBuffer())
-						.then((e) => Buffer.from(e)),
-			});
-
-			fonts = proxyResult.fonts;
-
-			await saveFontsToDisk({
-				filenameToContents: proxyResult.filenameToContents,
-				publicDir: paths.publicDir,
-				publicFontsDir: paths.publicFontsDir,
-				writeFile,
-				logger,
-			});
-
-			// TODO: ask for fallbacks (use resolved.fallbacks)
-			const fallbacks = resolved.fallbacks ?? [];
-
-			// TODO: optimized fallbacks
-
-			const css = generateCss({
-				cssVariable,
-				fallbacks,
-				family: family.name,
-				fonts,
-			});
-
-			await saveCssToDisk({
-				css,
-				cssVariable,
-				logger,
-				stylesDir: paths.stylesDir,
-				writeFile,
-			});
-
+		if (isAgent()) {
 			note(
 				[
-					"Now that you have font and CSS files, it is time to hook them up in your project.",
-					`Head over to the documentation to learn how: ${styleText("blue", "TODO:")}`,
+					"The main command is for human usage because it is interactive. Here is what to do:",
+					`1. Run ${pkg.name} search <family>, for example: ${pkg.name} search Inter. It will output the list of available font families`,
+					`2. Run ${pkg.name} details <family>, for example: ${pkg.name} details Inter. It will list all available properties: weights, styles, subsets and formats`,
+					`3. Run ${pkg.name} save --help to check all available parameters. Some should be provided from step 2`,
+					`4. Run ${pkg.name} save <family> [...args]`,
 				].join("\n"),
-				"Next steps",
+				"Agent instructions",
+				{ withGuide: false },
 			);
-
-			outro(outroMessage);
-		} catch (error) {
-			errorHandler.onError(error);
+			return;
 		}
+		return await import("./commands/main.js").then((mod) => mod.mainImpl());
 	},
 });
 
